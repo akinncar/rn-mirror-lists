d6049e82789c218ebd27b1e786e3a9540ec12c5b
akinncar <akinncar@hotmail.com>
2021-10-01 20:27:00 -0300

    feat: separate logic to lib

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,124 +1,79 @@
 import * as React from 'react';
 
-import { View, Text, Image, SafeAreaView, Dimensions, ScrollView } from 'react-native';
-import RnMirrorLists from 'rn-mirror-lists';
+import { View, Text, Image, SafeAreaView } from 'react-native';
+import { MirrorLists } from 'rn-mirror-lists';
 
-import useSWR from 'swr'
+import useSWR from 'swr';
 
-function Avatar({ item }) {
-  return <Image 
-    source={{ uri: item.image }}
-    style={{ 
-      width: 64, 
-      height: 64, 
-      marginHorizontal: 12,
-      marginVertical: 8,
-      borderRadius: 32, 
-    }}
-  />
-}
-
-function Information({ item }) {
-  return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Image 
+function Avatar({ item }: { item: Character }) {
+  return (
+    <Image
       source={{ uri: item.image }}
-      style={{ width: 64, height: 64, borderRadius: 32 }}
+      style={{
+        width: 64,
+        height: 64,
+        marginHorizontal: 12,
+        marginVertical: 8,
+        borderRadius: 32,
+      }}
     />
-    <Text style={{ padding: 16 }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</Text>
-  </View>
+  );
 }
+
+function Information({ item }: { item: Character }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Image
+        source={{ uri: item.image }}
+        style={{ width: 64, height: 64, borderRadius: 32 }}
+      />
+      <Text style={{ padding: 16 }}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum
+      </Text>
+    </View>
+  );
+}
+
+type Response = {
+  results: Array<Character>;
+};
+
+type Character = {
+  id: number;
+  image: string;
+};
 
 export default function App() {
-  const horizontalListRef = React.useRef(null)
-  const verticalListRef = React.useRef(null)
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-  const [horizontalItemWidth, setHorizontalItemWidth] = React.useState(0)
-  const [horizontalItemHeight, setHorizontalItemHeight] = React.useState(0)
-  
-  const [verticalListHeight, setVerticalListHeight] = React.useState(0)
+  const { data, error } = useSWR<Response>(
+    'https://rickandmortyapi.com/api/character',
+    fetcher
+  );
 
-  const [horizontalScrollActive, setHorizontalScrollActive] = React.useState(false)
-  const [verticalScrollActive, setVerticalScrollActive] = React.useState(false)
-
-  function onScrollHorizontal({ nativeEvent }) {
-    if (verticalScrollActive) return
-
-    const offset = (nativeEvent.contentOffset.x / horizontalItemWidth) * verticalListHeight;
-
-    verticalListRef.current.scrollTo({ 
-      animated: false, 
-      y: offset,
-    })
-  }
-
-  function onScrollVertical({ nativeEvent }) {
-    if (horizontalScrollActive) return
-
-    const offset = ((nativeEvent.contentOffset.y / verticalListHeight) * horizontalItemWidth);
-
-    horizontalListRef.current.scrollTo({ 
-      animated: false, 
-      x: offset,
-    })
-  }
-
-  function onHorizontalItemLayout({ nativeEvent }) {
-    if(!horizontalItemWidth && !horizontalItemHeight) {
-      setHorizontalItemWidth(nativeEvent.layout.width)
-      setHorizontalItemHeight(nativeEvent.layout.height)
-    }
-  }
-
-  const fetcher = (url: string) => fetch(url).then(res => res.json())
-
-  const { data, error } = useSWR('https://rickandmortyapi.com/api/character', fetcher)
-
-  if (error) return <Text>Failed to load</Text>
-  if (!data) return <Text>loading...</Text>
+  if (error) return <Text>Failed to load</Text>;
+  if (!data) return <Text>loading...</Text>;
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView
-        ref={horizontalListRef}
-        style={{ flexGrow: 0.05 }}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        onScroll={onScrollHorizontal}
-        onScrollBeginDrag={() => setHorizontalScrollActive(true)}
-        onMomentumScrollEnd={() => setHorizontalScrollActive(false)}
-        scrollEventThrottle={1}
-        decelerationRate={"fast"}
-        snapToInterval={horizontalItemWidth}
-      >
-          <View style={{ width: Dimensions.get('window').width * 0.5 - (horizontalItemWidth / 2) }} /> 
-            {data.results.map(item => {
-              return (
-                <View key={item.id.toString()} onLayout={onHorizontalItemLayout}>
-                  <Avatar item={item} />
-                </View>
-              )
-            })}
-          <View style={{ width: Dimensions.get('window').width * 0.5 - (horizontalItemWidth / 2) }} />
-      </ScrollView>
-      <ScrollView
-        ref={verticalListRef}
-        style={{ flex: 0.95 }}
-        onLayout={({ nativeEvent }) => setVerticalListHeight(nativeEvent.layout.height)}
-        onScroll={onScrollVertical}
-        onScrollBeginDrag={() => setVerticalScrollActive(true)}
-        onMomentumScrollEnd={() => setVerticalScrollActive(false)} 
-        scrollEventThrottle={1}
-        snapToInterval={verticalListHeight}
-        decelerationRate={"fast"}
-      >
-        {data.results.map(item => {
-          return (
-            <View style={{ height: verticalListHeight }} key={item.id.toString()}>
-              <Information item={item} />
-            </View>
-          )
-        })}  
-      </ScrollView>
+      <MirrorLists
+        data={data.results}
+        keyExtractor={({ item }: { item: Character }) => item.id.toString()}
+        horizontalRenderItem={({ item }: { item: Character }) => (
+          <Avatar item={item} />
+        )}
+        verticalRenderItem={({ item }: { item: Character }) => (
+          <Information item={item} />
+        )}
+      />
     </SafeAreaView>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,135 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
-};
+import * as React from 'react';
+
+import {
+  View,
+  Dimensions,
+  ScrollView,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+  LayoutChangeEvent,
+} from 'react-native';
+
+interface MirrorListsProps {
+  data: Array<any>;
+  horizontalRenderItem(item: any): JSX.Element;
+  verticalRenderItem(item: any): JSX.Element;
+  keyExtractor(item: any): React.Key;
+}
+
+export function MirrorLists({
+  data,
+  horizontalRenderItem,
+  verticalRenderItem,
+  keyExtractor,
+}: MirrorListsProps) {
+  const horizontalListRef = React.useRef<ScrollView>(null);
+  const verticalListRef = React.useRef<ScrollView>(null);
+
+  const [horizontalItemWidth, setHorizontalItemWidth] = React.useState(0);
+  const [horizontalItemHeight, setHorizontalItemHeight] = React.useState(0);
+
+  const [verticalListHeight, setVerticalListHeight] = React.useState(0);
+
+  const [horizontalScrollActive, setHorizontalScrollActive] =
+    React.useState(false);
+  const [verticalScrollActive, setVerticalScrollActive] = React.useState(false);
+
+  function onScrollHorizontal({
+    nativeEvent,
+  }: NativeSyntheticEvent<NativeScrollEvent>) {
+    if (verticalScrollActive) return;
+
+    const offset =
+      (nativeEvent.contentOffset.x / horizontalItemWidth) * verticalListHeight;
+
+    verticalListRef?.current?.scrollTo({
+      animated: false,
+      y: offset,
+    });
+  }
+
+  function onScrollVertical({
+    nativeEvent,
+  }: NativeSyntheticEvent<NativeScrollEvent>) {
+    if (horizontalScrollActive) return;
+
+    const offset =
+      (nativeEvent.contentOffset.y / verticalListHeight) * horizontalItemWidth;
+
+    horizontalListRef?.current?.scrollTo({
+      animated: false,
+      x: offset,
+    });
+  }
+
+  function onHorizontalItemLayout({ nativeEvent }: LayoutChangeEvent) {
+    if (!horizontalItemWidth && !horizontalItemHeight) {
+      setHorizontalItemWidth(nativeEvent.layout.width);
+      setHorizontalItemHeight(nativeEvent.layout.height);
+    }
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView
+        ref={horizontalListRef}
+        style={{ flexGrow: 0.05 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onScroll={onScrollHorizontal}
+        onScrollBeginDrag={() => setHorizontalScrollActive(true)}
+        onMomentumScrollEnd={() => setHorizontalScrollActive(false)}
+        scrollEventThrottle={1}
+        decelerationRate={'fast'}
+        snapToInterval={horizontalItemWidth}
+      >
+        <View
+          style={{
+            width:
+              Dimensions.get('window').width * 0.5 - horizontalItemWidth / 2,
+          }}
+        />
+        {data.map((item) => {
+          return (
+            <View
+              key={keyExtractor({ item })}
+              onLayout={onHorizontalItemLayout}
+            >
+              {horizontalRenderItem({ item })}
+            </View>
+          );
+        })}
+        <View
+          style={{
+            width:
+              Dimensions.get('window').width * 0.5 - horizontalItemWidth / 2,
+          }}
+        />
+      </ScrollView>
+      <ScrollView
+        ref={verticalListRef}
+        style={{ flex: 0.95 }}
+        onLayout={({ nativeEvent }: LayoutChangeEvent) =>
+          setVerticalListHeight(nativeEvent.layout.height)
+        }
+        onScroll={onScrollVertical}
+        onScrollBeginDrag={() => setVerticalScrollActive(true)}
+        onMomentumScrollEnd={() => setVerticalScrollActive(false)}
+        scrollEventThrottle={1}
+        snapToInterval={verticalListHeight}
+        decelerationRate={'fast'}
+      >
+        {data.map((item) => {
+          return (
+            <View
+              key={keyExtractor({ item })}
+              style={{ height: verticalListHeight }}
+            >
+              {verticalRenderItem({ item })}
+            </View>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+}
